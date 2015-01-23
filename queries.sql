330ccdedf176bc1a0dda8df2fabe953a57911121
redgaskell <red@everlane.com>
2015-01-22 16:21:35 -0800

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -14,6 +14,22 @@
 -- questons you have, or anything else.  The more we know about how you're
 -- thinking the better feedback we'll be able to give!
 
+--- Below are a bunch of "plain English" sentences.  Your job is to take a stab
+-- at translating each one into a single SELECT query.  You can write your
+-- query below each "plain English" sentence.
+--
+-- We've written some example queries as a way to both show you how and where
+-- to type your work and also as a way to show you new SQL commands, clauses,
+-- or syntax where necessary.
+
+-- **Please** don't delete the "plain English" versions. Just write your query
+-- on the line below it. :)
+
+-- The lines in this file that begin with "--" are comments.  You're encouraged
+-- to leave your own comments, reflecting on things you're confused about,
+-- questons you have, or anything else.  The more we know about how you're
+-- thinking the better feedback we'll be able to give!
+
 -- "albums" table
 
 -- Every album
@@ -36,39 +52,51 @@ SELECT * FROM albums WHERE title LIKE 'A%' ORDER BY title;
 -- Remember: run ".schema invoices" to see the structure of the "invoices" table
 
 -- Every invoice
+SELECT * FROM invoices;
 
 -- Every invoice ordered by total invoice amount ("total")
+SELECT * FROM invoices ORDER BY total DESC;
 
 -- Every invoice with a total greater than 10
+SELECT * FROM invoices WHERE total > 10;
 
 -- The 10 least expensive invoices
 -- Remember: ORDER BY orders from lowest-to-highest by default
 SELECT * FROM invoices ORDER BY total LIMIT 10;
 
 -- The 10 most expensive invoices
+SELECT * FROM invoices ORDER BY total DESC LIMIT 10;
 
 -- The 15 most recent invoices
+SELECT * FROM invoices ORDER BY invoice_date DESC LIMIT 15;
 
 -- The 15 oldest invoices
+SELECT * FROM invoices ORDER BY invoice_date ASC LIMIT 15;
 
 -- The 10 most expensive invoices from the US
+SELECT * FROM invoices WHERE billing_country="USA" ORDER BY total DESC LIMIT 10;
+
 
 -- The 10 least expensive invoices from the US
+SELECT * FROM invoices WHERE billing_country = "USA" ORDER BY total DESC LIMIT 10;
 
 -- The 10 most expensive invoices from outside the US
 -- Hint: If "=" means equal, use "!=" to mean "not equal"
+SELECT * FROM invoices WHERE billing_country != "USA" ORDER BY total DESC LIMIT 10;
 
 -- Every invoice from Chicago, IL
-SELECT * FROM invoices WHERE billing_city = "Chicago" AND billing_state = "IL" AND billing_country="USA";
+SELECT * FROM invoices WHERE billing_city = "Chicago" AND billing_state = "IL" AND billing_country = "USA";
 
 -- A list of all the invoices worth more than $5.00 from Chicago, IL
+SELECT * FROM invoices WHERE billing_city = "Chicago" AND billing_state = "IL" AND billing_country = "USA" AND total < 5;
 
 -- The billing addresses of the 5 most valuable invoices from Mountain View CA
 -- Gotta reward those big spenders!
+SELECT * FROM invoices WHERE billing_city = "Mountain View" AND billing_state = "CA" AND billing_country = "USA" ORDER BY total DESC LIMIT 5;
 
 -- A list of the 10 most valuable invoices made before January 1st, 2010
 -- Hint: Dates are formatted like 'YYYY-MM-DD' and you can compare them using '<', '>', '<=' and '>='
-
+SELECT * FROM invoices WHERE invoice_date <= 2010-01-00 ORDER BY total DESC LIMIT 10;
 
 -- The number of invoices from Chicago, IL
 SELECT COUNT(*) FROM invoices WHERE billing_city = "Chicago" AND billing_state = "IL" AND billing_country = "USA";
@@ -89,9 +117,17 @@ SELECT billing_state, COUNT(*), SUM(total) FROM invoices WHERE billing_country =
 SELECT billing_state, COUNT(*), SUM(total), AVG(total) FROM invoices WHERE billing_country = "USA" and billing_state = "CA";
 
 -- The count, total, and average of invoice totals, grouped by state, ordered by average invoice total from highest-to-lowest
+SELECT billing_state, COUNT(*), SUM(total), AVG(total) FROM invoices WHERE billing_country = "USA" GROUP BY billing_state ORDER BY AVG(total) DESC;
 
 -- A list of the top 5 countries by number of invoices
+SELECT billing_country, COUNT(*) FROM invoices GROUP BY billing_country ORDER BY COUNT(*) DESC LIMIT 5;
+
+
 
 -- A list of the top 5 countries by gross/total invoice size
+SELECT billing_country, SUM(total) FROM invoices GROUP BY billing_country ORDER BY SUM(total) DESC LIMIT 5;
+
 
 -- A list of the top 5 countries by average invoice size
+SELECT billing_country, AVG(total) FROM invoices GROUP BY billing_country ORDER BY AVG(total) DESC LIMIT 5;
+
